4b974c7a69b0dec2f8b0b81d6ef1054afe8a717f
kvishanov <k.vishanov@ibs.bg>
2020-04-16 16:30:36 +0300

    added clock web component

---
+++ js/webComponents/clockWc.js
@@ -0,0 +1,33 @@
+const templateClock = document.createElement('template');
+templateClock.innerHTML = `
+	<style>
+	#clock{
+		font-size: 2em;
+	}
+	</style>
+	<div id="clock"></div>
+`;
+
+class Clock extends HTMLElement{
+	constructor(){
+		super();
+		this.attachShadow({mode: 'open'});
+		this.shadowRoot.appendChild(templateClock.content.cloneNode(true));
+        this.shadowRoot.getElementById('clock').innerHTML = Date.now();
+
+	}
+
+	connectedCallback(){
+        this.interval = window.setInterval(() => {
+			this.date = new Date().toLocaleTimeString('bg');
+            this.shadowRoot.getElementById('clock').innerHTML = this.date;
+        });
+	}
+
+	disconnectedCallback(){
+        window.clearInterval(this.interval);
+	}
+
+}
+
+window.customElements.define('clock-wc', Clock);
